054abbc6c4324c338e9185351ddf28546993a32b
hsinhan <xxx@xxx.xx>
2019-06-13 13:35:46 +0800

    加入ant design framework

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
+import Button from 'antd/lib/button';
+import './App.css';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Button type="primary">Button</Button>
       </div>
     );
   }
